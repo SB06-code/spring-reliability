300cc39236a949feb3a53cdd26414983bc26a50c
hagyutae <gted221@gmail.com>
2025-10-29 10:29:17 +0900

    init project

---
+++ src/main/java/codeit/sb06/reliability/SpringReliabilityApplication.java
@@ -0,0 +1,13 @@
+package codeit.sb06.reliability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReliabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReliabilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codeit/sb06/reliability/SpringReliabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package codeit.sb06.reliability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReliabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
